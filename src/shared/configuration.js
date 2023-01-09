636893bc2e0cf5ed9abe4c784f2f37422d8a6b09
Oleg_Klimenko <oleg_klimenko@epam.com>
2023-01-08 18:30:00 -0800

    Initial version

---
+++ src/shared/configuration.js
@@ -0,0 +1,7 @@
+class Configuration {
+
+    ITEM_COLLECTION_URL = "https://ba7e16db-560a-40ba-9fdc-2c9f2a439e0a.mock.pstmn.io/collectionItems";
+    //http://localhost:8080/collectionItems";
+}
+
+export default Configuration;
